94d9cbb29da9c4491de750c4be383de4d83f2b2c
806-11 <806-11@DESKTOP-N61E745>
2023-12-26 10:35:48 +0900

    저장분기확인

--- src/com/javaex/helloworld/Helloworld.java
+++ src/com/javaex/helloworld/Helloworld.java
@@ -1,5 +1,10 @@
 package com.javaex.helloworld;
 
 public class Helloworld {
+	public static void main(String[] args) {
+		
+		System.out.println("Helloworld");
+		System.out.println("안녕세상");
+	}
 
 }
